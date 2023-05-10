15179b380b51a00f1c8e7bc825c9e6a908cb5477
Spectrus <47930965+SpeccyGirl@users.noreply.github.com>
2023-05-09 22:04:27 -0500

    hehe

--- js/plugins/AdjustLineHeight.js
+++
@@ -1,5 +0,0 @@
-alias_Bitmap_drawText = Bitmap.prototype.drawText;
-Bitmap.prototype.drawText = function (text, x, y, maxWidth, lineHeight, align) {
-    y += -5;
-    alias_Bitmap_drawText.call(this, text, x, y, maxWidth, lineHeight, align);
-};
